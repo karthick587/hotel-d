85b06e07b3261858087997503f7e60c3ff53fa59
karthick587 <85030277+karthick587@users.noreply.github.com>
2021-11-06 00:44:01 +0530

    gg

--- subscriberform.php
+++ subscriberform.php
@@ -1,6 +1,6 @@
 <?php
 if($_POST["Message"]) {
-mail("john@johndoe.com", "Here is the sample subject line",
-$_POST["Insert Your Message"]. "From: jane@janedoe.com");
+mail("karthickdurai587@gmail.com", "Here is the sample subject line",
+$_POST["Insert Your Message"]. "From: karthickdurai587@gmail.com");
 }
 ?>
